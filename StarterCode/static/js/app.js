9119868cd89d5ed976d3c181541ca5d55707f996
unknown <paoestefan@hotmail.com>
2021-01-18 18:23:28 -0600

    Adding code for the Demo-Metadata information

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -54,3 +54,15 @@ function buildCharts(sample) {
     });
 }
 
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+      PANEL.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
+    });
+  }
